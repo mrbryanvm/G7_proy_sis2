3a6f284bd672045d683c9832e10b7731c37af3cc
jhulZ304 <201900418@est.umss.edu>
2025-01-28 22:56:33 -0400

    cambios

---
+++ CurvearTextArea.java
@@ -0,0 +1,25 @@
+package biblioteca;
+import java.awt.*;
+import javax.swing.*;
+
+public class CurvearTextArea extends JTextArea {
+    public int radio;  // Radio de las esquinas redondeadas
+
+    // Constructor que recibe el radio de las esquinas redondeadas
+    public CurvearTextArea(int radio) {
+        this.radio = radio;
+        setOpaque(false); // Aseguramos que el área de texto sea transparente para que se vea el borde redondeado
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        
+        // Dibujar el borde con esquinas redondeadas
+        g2d.setColor(getBackground()); // Fondo del JTextArea
+        g2d.fillRoundRect(0, 0, getWidth(), getHeight(), radio, radio);
+        
+        super.paintComponent(g);
+    }
+}
---
+++ Plantilla.java
@@ -0,0 +1,76 @@
+package biblioteca;
+
+import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+
+public class Plantilla {
+
+
+    // crear boton
+    public static JButton crearBoton(String texto) {
+        CurvearButton boton = new CurvearButton(texto, 30);
+        
+        boton.setBackground(PaletaColor.COLORCELESTECLARO);  
+        boton.setForeground(PaletaColor.COLORBLANCO); 
+        boton.setFont(new Font("Arial", Font.BOLD, 18)); 
+        boton.setPreferredSize(new Dimension(170, 40)); 
+         return boton;
+    }
+
+    // crear texArea
+    public static CurvearTextArea crearTextArea() {
+        CurvearTextArea textArea = new CurvearTextArea(30); 
+        textArea.setBackground(PaletaColor.COLORPLOMO);  
+        textArea.setForeground(Color.BLACK);  
+        textArea.setFont(new Font("Arial", Font.PLAIN, 16)); 
+        textArea.setPreferredSize(new Dimension(180, 40)); //
+        return textArea;
+    }
+
+
+    public static CurvearPanel crearPanelBlanco(int width, int height) {
+        CurvearPanel panel = new CurvearPanel(25); 
+        panel.setBackground(PaletaColor.COLORBLANCO);   
+        panel.setPreferredSize(new Dimension(width, height));
+
+        return panel;
+    }
+
+    
+    public static JPanel crearPanelAzul() {
+        JPanel panel = new JPanel();  
+        panel.setBackground(PaletaColor.COLORAZUL);  
+        panel.setPreferredSize(new Dimension(850, 500));  
+       panel.setBounds(0, 100, 850, 500);
+        return panel;  
+    }
+
+
+    public static JPanel crearPanelNaranja() {
+        JPanel panel = new JPanel();
+        
+        panel.setBackground(PaletaColor.COLORNARANJA); 
+        panel.setPreferredSize(new Dimension(850, 100));
+          panel.setBounds(0, 0, 600, 100); 
+        
+        panel.setLayout(null); 
+          
+        JLabel logoLabel = new JLabel();
+        
+        ImageIcon logoIcono = new ImageIcon("biblioteca/imagenes/logo.png"); 
+        Image logoImage = logoIcono.getImage();
+        Image resizedImage = logoImage.getScaledInstance(150, 100, Image.SCALE_SMOOTH); 
+        logoLabel.setIcon(new ImageIcon(resizedImage));
+        logoLabel.setBounds(30, 0, 150, 100); 
+        panel.add(logoLabel);
+           
+        return panel;
+}
+}
+
+
