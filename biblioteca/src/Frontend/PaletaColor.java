5a5c4bda96d15b3fe238cf1a918a63b5bcc8c9d0
Conde12 <raulracec@gmail.com>
2025-01-30 00:00:58 -0400

    Mejoras en las clases y agregado nuevas clases

---
+++ biblioteca/src/Frontend/PaletaColor.java
@@ -0,0 +1,14 @@
+package Frontend;
+
+import java.awt.Color;
+
+public class PaletaColor {
+    
+    public  static final Color COLORPLOMO = Color.decode("#E3E3E4"); 
+    public static final Color COLORAZUL = Color.decode("#22348A"); 
+    public static final Color COLORNARANJA = Color.decode("#FFC165"); 
+    public  static final Color COLORBLANCO = Color.decode("#FBFBFB"); 
+    public  static final Color COLORCELESTE = Color.decode("#4A54AF"); 
+    public  static final Color COLORCELESTECLARO = Color.decode("#7274D4"); 
+
+}
